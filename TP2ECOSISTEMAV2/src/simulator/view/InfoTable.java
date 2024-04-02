20d49349d66c239d1d8986d33fabbe83505658fa
VaC306 <rishipursnani@gmail.com>
2024-04-02 22:32:20 +0200

    infotable

--- TP2ECOSISTEMAV2/src/simulator/view/InfoTable.java
+++ TP2ECOSISTEMAV2/src/simulator/view/InfoTable.java
@@ -1,6 +1,12 @@
 package simulator.view;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.TableModel;
 
 public class InfoTable extends JPanel {
@@ -13,9 +19,18 @@ public class InfoTable extends JPanel {
 	}
 	private void initGUI() {
 		// TODO cambiar el layout del panel a BorderLayout()
+		setLayout(new BorderLayout());
+		
 		// TODO añadir un borde con título al JPanel, con el texto _title
+		TitledBorder titleBorder = BorderFactory.createTitledBorder(_title);
+        setBorder(titleBorder);
+        
 		// TODO añadir un JTable (con barra de desplazamiento vertical) que use
 		// _tableModel
+        JTable table = new JTable(_tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane, BorderLayout.CENTER);
+        
 	}
 }
 
